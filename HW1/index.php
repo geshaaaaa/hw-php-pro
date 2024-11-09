a79d24f902616eb81a754122464c3ca1f4bbd005
Hena <gena200399@gmail.com>
2024-11-09 19:01:01 +0200

    idea git ignore

--- HW1/index.php
+++ HW1/index.php
@@ -1,3 +1,27 @@
 <?php
 
-echo "Hello! We created new branch for pull";
+class Contact
+
+{
+    protected string $name;
+
+    public function __set(string $name, $value): void
+    {
+        $this->name = $value;
+    }
+
+    protected function hiHello ($Yo)
+    {
+        echo "$Yo hello";
+    }
+
+    /*public function __call(string $name, array $arguments)
+    {
+        // TODO: Implement __call() method.
+    }*/
+}
+
+    $obj = new Contact();
+
+$obj->hiHello("Hena");
+
