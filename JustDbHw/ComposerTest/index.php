26f38518e2988e0fd4be406fbcc2ec887b39e8d9
Hena <gena200399@gmail.com>
2024-11-22 21:03:04 +0200

    hide vendor

--- JustDbHw/ComposerTest/index.php
+++ JustDbHw/ComposerTest/index.php
@@ -1,6 +1,22 @@
 <?php
 require_once "vendor/autoload.php";
 
-d($_SERVER);
+$dsn = 'mysql:host=mysql;dbname = php_10_24_db;port = 3306';
+$user = 'root';
+$password = 'secret';
 
-echo "test";
+$pdo = new PDO($dsn,$user,$password);
+
+$opts = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+
+    $pdo = new PDO($dsn,$user,$password);
+}
+catch (PDOException $exception)
+{
+    var_dump($exception->getMessage());
+}
